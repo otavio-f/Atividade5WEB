d6c4f6992ac861e9f705fe35c75aa5f016c49169
otavi <otavi@RADINHO-2>
2020-09-21 21:06:03 -0300

    Limpeza

--- src/main/java/br/edu/uepb/jaxrsexample/Main.java
+++ src/main/java/br/edu/uepb/jaxrsexample/Main.java
@@ -6,7 +6,6 @@ import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import org.glassfish.grizzly.http.server.HttpServer;
 
-import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.net.URI;
 
