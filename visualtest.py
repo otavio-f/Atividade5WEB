8daf8772450011b7a37fe681c582d8e0f2da0cd4
Otavio F <38397566+otavio-f@users.noreply.github.com>
2020-09-21 21:45:17 -0300

    Update visualtest.py

--- visualtest.py
+++ visualtest.py
@@ -10,12 +10,12 @@ url = "http://localhost:8080/api/"
 test_point = "turmas"
 dados = {"id": 0, "nome":"Musica", "turno":"noturno"}
 
-print(f"POST para {url+test_point} com dados {dados}")
+print(f"\nPOST para {url+test_point} com dados {dados}")
 response = requests.post(url+test_point, json=dados)
 print(f"{response.status_code}: {response.text}")
 
 dados2 = {"id": 0, "nome":"Teatro", "curso":"diurno"}
-print(f"POST para {url+test_point} com dados {dados2}")
+print(f"\nPOST para {url+test_point} com dados {dados2}")
 response = requests.post(url+test_point, json=dados2)
 print(f"{response.status_code}: {response.text}")
 
@@ -24,8 +24,24 @@ response = requests.get(url+test_point)
 print(f"{response.status_code}: {response.text}")
 
 test_point = "turmas/1"
+print(f"\nGET para {url+test_point}")
+response = requests.get(url+test_point)
+print(f"{response.status_code}: {response.text}")
+dados = response.json()
+dados["nome"] = "Atletismo"
+
+print(f"\nPUT para {url+test_point}")
+response = requests.put(url+test_point, json=dados)
+print(f"{response.status_code}: {response.text}")
+
+test_point = "turmas"
+print(f"\nGET para {url+test_point}")
+response = requests.get(url+test_point)
+print(f"{response.status_code}: {response.text}")
+
+test_point = "turmas/1"
 print(f"\nDELETE para {url+test_point}")
-response = requests.delete(url+test_point, json=dados)
+response = requests.delete(url+test_point)
 print(f"{response.status_code}: {response.text}")
 
 test_point = "turmas"
@@ -43,7 +59,7 @@ response = requests.post(url+test_point, json=dados)
 print(f"{response.status_code}: {response.text}")
 
 dados2 = {"id": 0, "nome":"Maria", "curso":"algum"}
-print(f"POST para {url+test_point} com dados {dados2}")
+print(f"\nPOST para {url+test_point} com dados {dados2}")
 response = requests.post(url+test_point, json=dados2)
 print(f"{response.status_code}: {response.text}")
 
@@ -52,8 +68,24 @@ response = requests.get(url+test_point)
 print(f"{response.status_code}: {response.text}")
 
 test_point = "alunos/1"
+print(f"\nGET para {url+test_point}")
+response = requests.get(url+test_point)
+print(f"{response.status_code}: {response.text}")
+dados = response.json()
+dados["nome"] = "Joao"
+
+print(f"\nPUT para {url+test_point}")
+response = requests.put(url+test_point, json=dados)
+print(f"{response.status_code}: {response.text}")
+
+test_point = "alunos"
+print(f"\nGET para {url+test_point}")
+response = requests.get(url+test_point)
+print(f"{response.status_code}: {response.text}")
+
+test_point = "alunos/1"
 print(f"\nDELETE para {url+test_point}")
-response = requests.delete(url+test_point, json=dados)
+response = requests.delete(url+test_point)
 print(f"{response.status_code}: {response.text}")
 
 test_point = "alunos"
