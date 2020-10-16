602d413c1944bb0bd30e2aeef805672708d9c844
otavi <otavi@RADINHO-2>
2020-10-16 19:26:38 -0300

    Saida traduzida

--- visualtest.py
+++ visualtest.py
@@ -13,116 +13,133 @@ def delete_all(endpoint: str):
 
 # T U R M A S #
 
+print("Limpando base de dados... ", end="")
+delete_all(url+"turmas/")
+delete_all(url+"alunos/")
+print("Feito!")
+
 endpoint = url+"turmas/"
-print("Resetting database... ", end="")
-delete_all(endpoint)
-print("Done!")
 
+# Insert #
 dados = {"id": 0, "nome":"Musica", "turno":"noturno"}
-
-print(f"\nPOST para {endpoint} com dados {dados}")
+print(f"\nInserindo uma turma... ", end="")
 response = requests.post(endpoint, json=dados)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 201
+response_data = response.json()
+assert response_data == {"id": 1, "nome":"Musica", "turno":"noturno"}
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos: \n\t{response_data}")
 
-dados2 = {"id": 0, "nome":"Teatro", "curso":"diurno"}
-print(f"\nPOST para {endpoint} com dados {dados2}")
-response = requests.post(endpoint, json=dados2)
-print(f"{response.status_code}: {response.text}")
+# Insert #
+dados = {"id": 0, "nome":"Teatro", "turno":"diurno"}
+print(f"\nInserindo uma turma... ", end="")
+response = requests.post(endpoint, json=dados)
 assert response.status_code == 201
+response_data = response.json()
+assert response_data == {"id": 2, "nome":"Teatro", "turno":"diurno"}
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos: \n\t{response_data}")
 
-print(f"\nGET para {endpoint}")
+# Get all turmas #
+print("\nRecuperando dados de todas as turmas... ", end="")
 response = requests.get(endpoint)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 200
+response_data = response.json()
+assert len(response_data) == 2
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos:\n\t\t{response_data[0]}\n\t\t{response_data[1]}")
 
-endpoint += "1"
-print(f"\nGET para {endpoint}")
+# Get turma/1 #
+endpoint = url+"turmas/1"
+print("\nRecuperando dados de uma turma... ", end="")
 response = requests.get(endpoint)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 200
+response_data = response.json()
+assert response_data == {"id": 1, "nome":"Musica", "turno":"noturno"}
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos:\n\t{response_data}")
 
+# Modify turma/1 #
 dados = response.json()
 dados["nome"] = "Atletismo"
-print(f"\nPUT para {endpoint}")
+print("\nModificando turma anterior... ", end="")
 response = requests.put(endpoint, json=dados)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 200
+response_data = response.json()
+assert response_data == {"id": 1, "nome":"Atletismo", "turno":"noturno"}
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos:\n\t{response_data}")
 
-endpoint = url+"turmas/"
-print(f"\nGET para {endpoint}")
-response = requests.get(endpoint)
-print(f"{response.status_code}: {response.text}")
-assert response.status_code == 200
-
-endpoint += "1"
-print(f"\nDELETE para {endpoint}")
+# Delete turma/1 #
+endpoint = url+"turmas/1"
+print("\nDeletando uma turma... ", end="")
 response = requests.delete(endpoint)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 200
-
+# Check if turma count is 1 #
 endpoint = url+"turmas/"
-print(f"\nGET para {endpoint}")
-response = requests.get(endpoint)
-print(f"{response.status_code}: {response.text}")
-assert response.status_code == 200
+response = requests.get(url+"turmas/")
+response_data = response.json()
+assert len(response_data) == 1
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados restantes:\n\t{response_data}")
 
 # A L U N O S #
 
 endpoint = url+"alunos/"
 
+# Insert #
 dados = {"id": 0, "nome":"Jose", "curso":"outro"}
-print(f"\nPOST para {endpoint} com dados {dados}")
+print(f"\nInserindo um aluno... ", end="")
 response = requests.post(endpoint, json=dados)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 201
+response_data = response.json()
+assert response_data == {"id": 1, "nome":"Jose", "curso":"outro"}
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos: \n\t{response_data}")
 
-dados2 = {"id": 0, "nome":"Maria", "curso":"algum"}
-print(f"\nPOST para {endpoint} com dados {dados2}")
-response = requests.post(endpoint, json=dados2)
-print(f"{response.status_code}: {response.text}")
+# Insert #
+dados = {"id": 0, "nome":"Maria", "curso":"algum"}
+print(f"\nInserindo um aluno... ", end="")
+response = requests.post(endpoint, json=dados)
 assert response.status_code == 201
+response_data = response.json()
+assert response_data == {"id": 2, "nome":"Maria", "curso":"algum"}
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos: \n\t{response_data}")
 
-print(f"\nGET para {endpoint}")
+# Get all alunos #
+print("\nRecuperando dados de todos os alunos... ", end="")
 response = requests.get(endpoint)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 200
+response_data = response.json()
+assert len(response_data) == 2
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos:\n\t\t{response_data[0]}\n\t\t{response_data[1]}")
 
-endpoint += "1"
-print(f"\nGET para {endpoint}")
+# Get aluno/1 #
+endpoint = url+"alunos/1"
+print("\nRecuperando dados de um aluno... ", end="")
 response = requests.get(endpoint)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 200
+response_data = response.json()
+assert response_data == {"id": 1, "nome":"Jose", "curso":"outro"}
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos:\n\t{response_data}")
 
+# Modify aluno/1 #
 dados = response.json()
 dados["nome"] = "Joao"
-print(f"\nPUT para {endpoint}")
+print("\nModificando aluno anterior... ", end="")
 response = requests.put(endpoint, json=dados)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 200
+response_data = response.json()
+assert response_data == {"id": 1, "nome":"Joao", "curso":"outro"}
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados recebidos:\n\t{response_data}")
 
-endpoint = url+"alunos/"
-print(f"\nGET para {endpoint}")
-response = requests.get(endpoint)
-print(f"{response.status_code}: {response.text}")
-assert response.status_code == 200
-
-endpoint += "1"
-print(f"\nDELETE para {endpoint}")
+# Delete aluno/1 #
+endpoint = url+"alunos/1"
+print("\nDeletando um aluno... ", end="")
 response = requests.delete(endpoint)
-print(f"{response.status_code}: {response.text}")
 assert response.status_code == 200
+# Check if aluno count is 1 #
+response = requests.get(url+"alunos/")
+response_data = response.json()
+assert len(response_data) == 1
+print(f"OK!\n\tCodigo {response.status_code}.\n\tDados restantes:\n\t{response_data}")
 
-endpoint = url+"alunos/"
-print(f"\nGET para {endpoint}")
-response = requests.get(endpoint)
-print(f"{response.status_code}: {response.text}")
-assert response.status_code == 200
-
-print("Cleaning up database... ", end="")
+print("\n\nLimpando base de dados... ", end="")
 delete_all(url+"turmas/")
 delete_all(url+"alunos/")
-print("Done!")
-
+print("Feito!")
 
 input()
